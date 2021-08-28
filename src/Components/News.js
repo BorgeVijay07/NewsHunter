5f2df5783cb642e2115c52c04d70c6f10a0205dd
BorgeVijay07 <borgevijay11@gmail.com>
2021-08-28 18:46:56 +0530

    Added free news Api for news fetching with next and previous buttons.

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -7,27 +8,61 @@ export class News extends Component {
         super();
         this.state = {
             articles : [],
-            loading : false 
+            loading : false,
+            page : 1
         }
     }
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=5716ebcff2884536a813eaa6042b94c7";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5716ebcff2884536a813eaa6042b94c7&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
         let data = await fetch(url);
         let parseData = await data.json();
-        this.setState({articles: parseData.articles})
+        this.setState({
+            articles: parseData.articles,
+            totalResults: parseData.totalResults,
+            loading: false
+        })
+    }
+
+    handleNextClick = async() => {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5716ebcff2884536a813eaa6042b94c7&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            this.setState({loading: true});
+            let data = await fetch(url);
+            let parseData = await data.json();
+            this.setState({
+                page: this.state.page + 1,
+                articles: parseData.articles,
+                loading: false
+            })
+    }
+    handlePrevClick = async() => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5716ebcff2884536a813eaa6042b94c7&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
+        let data = await fetch(url);
+        let parseData = await data.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: parseData.articles,
+            loading: false
+        })
     }
 
     render() {
         return (
             <div className="container my-3">
-                <h2>NewsHunter - Top Headlines</h2>
+                <h1 className="text-center">NewsHunter - Top Headlines</h1>
+                {this.state.loading && <Spinner/>}
                 <div className="row">
-                    {this.state.articles.map((element)=>{
+                    {!this.state.loading && this.state.articles.map((element)=>{
                         return <div className="col-md-4" key={element.url}>
                         <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0,88):""} imgUrl={element.urlToImage} newsUrl={element.url}/>
                         </div>
                     })}
                 </div>
+                {!this.state.loading && <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page<=1} type="button" className="btn btn-dark btn-sm" onClick={this.handlePrevClick}>&larr; Previous</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark btn-sm" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>}
             </div>
         )
     }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -13,7 +13,7 @@ export class NewsItem extends Component {
                         <div className="card-body">
                             <h5 className="card-title">{title}...</h5>
                             <p className="card-text">{description}...</p>
-                            <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">View More</a>
+                            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark btn-sm">View More</a>
                         </div>
                     </div>
                 </div>
