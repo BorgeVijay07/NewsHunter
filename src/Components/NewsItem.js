286f1868a4e39cb6341188ba551246cc8da84fff
BorgeVijay07 <borgevijay11@gmail.com>
2021-08-29 12:39:57 +0530

    Added author, date, time and source in news cards.

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,14 +5,16 @@ export class NewsItem extends Component {
     
 
     render() {
-        let {title, description, imgUrl, newsUrl} = this.props;
+        let {title, description, imgUrl, newsUrl, author, date, source} = this.props;
         return (
             <>  <div className='my-3'>
                     <div className="card">
+                        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>{source}</span>
                         <img src={!imgUrl?"https://images.moneycontrol.com/static-mcnews/2021/08/shutterstock_527211817-770x433.jpg":imgUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
+                            <p className="card-text"><small className="text-muted">By {!author?"unknown":author} on {new Date(date).toGMTString()}</small></p>
                             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark btn-sm">View More</a>
                         </div>
                     </div>
